b0c77af475c7ded3e0318c1ec5b6a40f6488e20a
octo <dlqhdgkr456@gmail.com>
2022-02-15 15:16:20 +0900

    AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,20 +1,30 @@
 package hello.core;
 
+import hello.core.dicount.DiscountPolicy;
 import hello.core.dicount.FixDiscountPolicy;
 import hello.core.memberService.MemberService;
 import hello.core.memberService.MemberServiceImpl;
 import hello.core.orderService.OrderService;
 import hello.core.orderService.OrderServiceImpl;
+import hello.core.repository.MemberRepository;
 import hello.core.repository.MemoryMemberRepository;
 
 public class AppConfig {
 
     // 생성자를 통해 객체가 들어감 (생성자 주입, DI)
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
