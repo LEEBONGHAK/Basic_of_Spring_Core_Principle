2c1c5a05c995cd324151ae2c27fb3034f619577b
octo <dlqhdgkr456@gmail.com>
2022-02-16 17:46:57 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -18,16 +18,19 @@ public class AppConfig {
     // 생성자를 통해 객체가 들어감 (생성자 주입, DI)
     @Bean   // 스프링 컨테이너에 등록됨
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
